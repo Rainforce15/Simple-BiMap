b85460a568f97af0c2a2d85409472dfe48b2033e
Rainforce15 <rainforce15@gmail.com>
2017-07-29 15:11:28 +0200

    fixed up code

--- simple-bimap.js
+++ simple-bimap.js
@@ -1,49 +1,94 @@
-var Bimap = exports = function(isComplex){
+var BiMap = function(ignoreProxyWarning){
 
-    var bimap = {};
+    var biMap = {};
+    var revMap = {};
 
-    var mapOps = {
+    function escape(key) {
+        var keyProp = key;
+        if (typeof keyProp === "object") {
+            keyProp = JSON.stringify(keyProp);
+        }
+        if (typeof keyProp === "function") {
+            keyProp = keyProp.toString();
+        }
+        return keyProp;
+    }
 
+    var proxOps = {
         set: function set(target, prop, val) {
-
-            target[prop] = val;
-            if (typeof val !== "object") {
-                target[val] = prop;
+            if (biMap[escape(prop)]) {
+                this.deleteProperty(target, prop);
             }
+            if (biMap[escape(val)]) {
+                this.deleteProperty(target, val);
+            }
+            biMap[escape(prop)] = val;
+            revMap[escape(val)] = prop;
+        },
 
-            return true;
+        get: function get(target, prop) {
+            if (Object.keys(mapOps).indexOf(prop) === -1) {
+                prop = escape(prop);
+                if (typeof biMap[prop] !== "undefined") {
+                    return biMap[prop];
+                }
+                else if (typeof revMap[prop] !== "undefined") {
+                    return revMap[prop];
+                } else {
+                    return undefined;
+                }
+            } else {
+                return biMap[prop];
+            }
         },
 
         deleteProperty: function deleteProperty(target, prop) {
-
-            delete target[target[prop]];
-            delete target[prop];
-
-            return true;
+            prop = escape(prop);
+            if (biMap[prop]) {
+                delete revMap[biMap[prop]];
+                delete biMap[prop];
+                return true;
+            } else if (revMap[prop]) {
+                delete biMap[revMap[prop]];
+                delete revMap[prop];
+                return true;
+            } else {
+                return false;
+            }
         }
-
     };
 
-    Object.defineProperty(bimap, "_set", {
-        value: function(prop, val) {
-            mapOps.set(this, prop, val);
-        }
-    });
+    var mapOps = {
+        _set: function(prop, val) {
+            return proxOps.set(this, prop, val);
+        },
+        _get: function(prop) {
+            return proxOps.get(this, prop);
+        },
+        _del: function(prop) {
+            return proxOps.deleteProperty(this, prop);
+        },
+        _keys: function() {
+            return Object.keys(biMap).concat(Object.keys(revMap))
+        },
+        _rev: revMap
+    };
 
-    Object.defineProperty(bimap, "_del", {
-        value: function(prop) {
-            mapOps.deleteProperty(this, prop);
-        }
+    Object.keys(mapOps).forEach(function(key){
+        Object.defineProperty(biMap, key, {
+            value: mapOps[key]
+        });
     });
 
     if (typeof Proxy !== "undefined") {
-
-        return new Proxy(bimap, mapOps);
-
-    } else {
-
+        return new Proxy(biMap, proxOps);
+    } else if (!ignoreProxyWarning) {
         console.warn("could not create simple BiMap: requires Proxy");
-
     }
-    return bimap;
+
+    return biMap;
 };
+
+if (module) {
+    module.exports = BiMap;
+}
